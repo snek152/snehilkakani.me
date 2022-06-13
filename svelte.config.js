34d9bacf21d0f7994eb297e51c8863dd3c8503d1
snek152 <kakanisnehil@gmail.com>
2022-06-13 08:00:52 +0530

    adding back adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import viteCompression from 'vite-plugin-compression';
 
@@ -9,7 +9,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		vite: {
 			plugins: [viteCompression()]
 		}
