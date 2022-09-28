1ba263649df8d5f4ce873fb616e632c3f5cfac5e
snek152 <kakanisnehil@gmail.com>
2022-09-27 18:21:15 -0700

    adding something questionable

---
+++ src/routes/info.svelte
@@ -0,0 +1,9 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	export const load: Load = async () => {
+		return {
+			redirect: 'https://youtu.be/dQw4w9WgXcQ',
+			status: 308
+		};
+	};
+</script>
