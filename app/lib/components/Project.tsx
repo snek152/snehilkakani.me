393f54718abd1882741a85906679e024f63e9d87
snek152 <kakanisnehil@gmail.com>
2025-08-08 11:35:07 -0700

    adding a new project animation

--- app/lib/components/Project.tsx
+++ app/lib/components/Project.tsx
@@ -4,6 +4,7 @@ import { Project as ProjectT } from "../data/projects";
 import { ArrowUpRightIcon } from "@heroicons/react/16/solid";
 import React from "react";
 import { SiGithub } from "@icons-pack/react-simple-icons";
+import * as motion from "motion/react-m";
 // import { motion } from "motion/react";
 
 /*
@@ -22,84 +23,73 @@ const Project = React.memo(({ project }: { project: ProjectT }) => {
       className="w-full border-secondary group/project bg-background relative p-0 rounded-xl overflow-hidden border-[1.5px] shadow-lg"
     >
       {/* Header bar */}
-      <div className="flex items-center justify-between gap-2 px-2 lg:px-4 py-1 lg:py-2 bg-secondary/70 backdrop-blur-xs shadow-lg absolute top-2 right-2 z-30 text-primary border-2 border-primary/5 rounded-2xl">
-        {/* <span className="w-3 h-3 rounded-full bg-surface" />
-        <span className="w-3 h-3 rounded-full bg-surface" />
-        <span className="w-3 h-3 rounded-full bg-surface" /> */}
-        {/* <span className="text-xs font-mono text-surface/60">
-          projects/{project.slug}.tsx
-        </span> */}
+      <motion.div
+        initial={{ opacity: 0, x: 20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="flex items-center justify-between gap-2 px-2 lg:px-4 py-1 lg:py-2 bg-secondary/70 backdrop-blur-xs shadow-lg absolute top-2 right-2 z-30 text-primary border-2 border-primary/5 rounded-2xl"
+      >
         {project.github && (
-          <a
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
             href={project.github}
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center justify-center flex-shrink-0 w-6 h-6 text-surface hover:text-primary rounded-full transition-colors duration-300 text-xs"
             title="More information"
           >
-            {/* learn more */}
             <SiGithub className="w-full h-full" />
-          </a>
+          </motion.a>
         )}
         {project.link && (
-          <a
+          <motion.a
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
             href={project.link}
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center group/button justify-center flex-shrink-0 w-6 h-6 rounded-full transition-colors duration-300 text-xs bg-surface hover:bg-primary"
             title="More information"
           >
-            {/* learn more */}
             <ArrowUpRightIcon className="w-5 h-5 text-secondary/80 group-hover/button:text-surface transition-colors duration-300" />
-          </a>
+          </motion.a>
         )}
-      </div>
-      {/* Subtle grid background */}
-      {/* Main content */}
-      <Image
-        src={project.image}
-        alt={project.title}
-        // fill
-        // loading="lazy"
-        // quality={10}
-        placeholder="blur"
-        className="object-cover opacity-70 w-full h-64 group-hover/project:opacity-90 transition-opacity duration-300"
-      />
+      </motion.div>
+
+      <motion.div
+        initial={{ scale: 1.05, opacity: 0 }}
+        whileInView={{ scale: 1, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+      >
+        <Image
+          src={project.image}
+          alt={project.title}
+          placeholder="blur"
+          className="object-cover opacity-70 w-full h-64 group-hover/project:opacity-90 transition-opacity duration-300"
+        />
+      </motion.div>
+
       <div className="relative z-30 flex p-6 rounded-xl flex-col items-left text-left overflow-hidden">
-        {/* <div className="absolute inset-0 pointer-events-none opacity-10 z-0 rounded-xl overflow-hidden">
-              <svg width="100%" height="100%">
-                <defs>
-                  <pattern
-                    id="grid"
-                    width="24"
-                    height="24"
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <path
-                      d="M 24 0 L 0 0 0 24"
-                      fill="none"
-                      stroke="#fbfbfb"
-                      strokeWidth="0.5"
-                    />
-                  </pattern>
-                </defs>
-                <rect width="100%" height="100%" fill="url(#grid)" />
-              </svg>
-            </div> */}
         <h1 className="text-2xl font-bold tracking-tight text-surface mb-2 font-domine">
           <span className="text-primary">{"<"}</span>
           {project.title}
           <span className="text-primary">{"/>"}</span>
         </h1>
+
         {project.subtitle && (
           <h2 className="text-sm font-semibold text-surface/80 font-mono text-left mb-3">
             <span className="text-surface/80">{"// "}</span>
             {project.subtitle}
           </h2>
         )}
+
         <p className="text-surface text-base w-full mb-5 font-ibm leading-relaxed">
           {project.description}
         </p>
+
         <div className="flex flex-wrap gap-2 justify-left w-full">
           {project.skills.map(
             (skill, index) =>
