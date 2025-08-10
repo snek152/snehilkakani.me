1c19d49714a6507df4d26b181158786299f7c504
snek152 <kakanisnehil@gmail.com>
2025-08-10 16:50:09 -0700

    making the mac icon things smaller

--- app/lib/components/Tools.tsx
+++ app/lib/components/Tools.tsx
@@ -63,13 +63,13 @@ const ToolIcon = ({
 export default function Tools() {
   return (
     <Card className="col-span-2 p-0">
-      <div className="flex items-center gap-2 px-3 lg:px-4 py-2 lg:py-3 bg-background relative z-20 text-primary border-b border-secondary rounded-t-xl">
-        <span className="w-3 h-3 rounded-full bg-surface" />
-        <span className="w-3 h-3 rounded-full bg-surface" />
-        <span className="w-3 h-3 rounded-full bg-surface" />
-        <span className="ml-2 text-xs font-mono text-surface/60">
+      <div className="flex items-center gap-2 px-2 lg:px-3 py-1.5 lg:py-2.5 bg-background relative z-20 text-primary border-b border-secondary rounded-t-xl">
+        <span className="w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full bg-surface" />
+        <span className="w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full bg-surface" />
+        <span className="w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full bg-surface" />
+        {/* <span className="ml-1 text-xs font-mono text-surface/60">
           projects/tools.tsx
-        </span>
+        </span> */}
       </div>
       <div className="absolute inset-0 pointer-events-none opacity-10 z-0">
         <svg width="100%" height="100%">
