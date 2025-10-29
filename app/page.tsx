e60f4058ea3f3705f34b61e830511f32adae1e35
snek152 <kakanisnehil@gmail.com>
2025-10-29 15:20:05 -0700

    hiding experience section for now, updating resume

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // import HomeGallery from "./lib/components/HomeGallery";
 import HomePage from "./lib/components/HomePage";
-import Experience from "./lib/components/Experience";
+// import Experience from "./lib/components/Experience";
 // import { Metadata } from "next";
 // import LoadingWrapper from "./components/LoadingWrapper";
 
@@ -12,9 +12,9 @@ export default function Home() {
   return (
     <>
       <HomePage />
-      <div className="w-full grid grid-cols-1 lg:grid-cols-2">
+      {/* <div className="w-full grid grid-cols-1 lg:grid-cols-2">
         <Experience />
-      </div>
+      </div> */}
       {/* <HomeGallery /> */}
     </>
   );
