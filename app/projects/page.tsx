62c9ff53a01761d1f47827d831d0424654b2ead9
snek152 <kakanisnehil@gmail.com>
2026-04-29 16:52:02 -0700

    adding project header back

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,10 +15,10 @@ export default function About() {
   return (
     <div className="w-full grid gap-5 px-5 py-2 lg:py-5 grid-cols-2">
       <Tools />
-      {/* <h1 className="text-3xl col-span-2 lg:text-4xl font-bold text-center tracking-tight text-surface mb-2 font-domine">
+      <h1 className="text-3xl col-span-2 mt-7 lg:text-4xl font-bold text-center tracking-tight text-surface mb-2 font-domine">
         <span className="text-primary">{"<"}</span>Projects
         <span className="text-primary">{"/>"}</span>
-      </h1> */}
+      </h1>
       <Suspense
         fallback={<LoadingSpinner className="col-span-2 h-full mt-10" />}
       >
