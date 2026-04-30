17a378864daf559fd925d081e46b0135b926ee99
snek152 <kakanisnehil@gmail.com>
2026-04-29 18:19:20 -0700

    adding gallery header

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,8 +3,16 @@ import PhotoGallery from "../lib/components/FeaturedGallery";
 
 export const metadata: Metadata = {
   title: "Gallery",
+  description: "TODO",
 };
 
 export default function Gallery() {
-  return <PhotoGallery />;
+  return (
+    <div className="min-h-dvh px-2 py-2 lg:py-10">
+      <h1 className="text-3xl lg:text-4xl font-bold text-white mb-4 font-domine text-center">
+        Photography
+      </h1>
+      <PhotoGallery />
+    </div>
+  );
 }
