6f86c3bdb48127ca2b352fce8cef4fb160ed8fe6
snek152 <kakanisnehil@gmail.com>
2026-04-30 14:58:21 -0700

    move tools under projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,7 +17,6 @@ const AllProjects = dynamic(() => import("../lib/components/AllProjects"));
 export default function About() {
   return (
     <div className="w-full grid gap-5 px-5 py-2 lg:py-5 grid-cols-2">
-      <Tools />
       <h1 className="text-3xl col-span-2 mt-7 lg:text-4xl font-bold text-center tracking-tight text-surface mb-2 font-domine">
         <span className="text-primary">{"<"}</span>Projects
         <span className="text-primary">{"/>"}</span>
@@ -27,6 +26,7 @@ export default function About() {
       >
         <AllProjects />
       </Suspense>
+      <Tools />
     </div>
   );
 }
